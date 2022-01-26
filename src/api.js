cf2e97bbd3b617ff7172c5c036bcaf917202f9d7
XamzatJR <bahaev.hamzat95@gmail.com>
2022-01-26 20:43:08 +0300

    добавил поддержку кросс-курса

--- src/api.js
+++ src/api.js
@@ -2,20 +2,42 @@ const API_KEY = "fa2147371ff46aa9dd33a80399caaee4c0ac8ad502a502ed7d08893e764630c
 const socket = new WebSocket( `wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`);
 const tickersHandlers = new Map();
 export const invalidCoins = [];
-socket.addEventListener('message', (e) => {
-    const { TYPE: type, FROMSYMBOL: currency, PRICE: newPrice, MESSAGE: message, PARAMETER: param} = JSON.parse(e.data);
+const unsentCoins = [];
+let btc = null;
+socket.addEventListener('message', (e) => { 
+    const { TYPE: type, FROMSYMBOL: currency, PRICE: newPrice, MESSAGE: message, PARAMETER: param, TOSYMBOL} = JSON.parse(e.data);
     const handlers = tickersHandlers.get(currency) ?? [];
     if (message === "INVALID_SUB") {
         let coin = param.slice(9, param.lastIndexOf("~"));
-        invalidCoins.push(coin);
+        if (invalidCoins.includes(coin)) {
+            unsentCoins.splice(invalidCoins.indexOf(coin), 1);
+        } else {
+            invalidCoins.push(coin);
+            unsentCoins.push(coin);
+            crossRateCoins(coin);
+        }
     }
     if (type !== '5' || newPrice === undefined) {
         return ;
     }
-    
-    handlers.forEach(fn => fn(newPrice));
+    if (currency === "BTC") {
+        btc = newPrice;
+    }
+        if (TOSYMBOL === "BTC") {
+            handlers.forEach(fn => fn(newPrice*btc));
+        }else {
+            handlers.forEach(fn => fn(newPrice));
+        }
 });
 
+function crossRateCoins(ticker) {
+    sendToWebSocket(
+        {
+            "action": "SubAdd",
+            "subs": [`5~CCCAGG~${ticker}~BTC`]
+        }
+    )
+}
 
 function sendToWebSocket(message) {
     const stringifiedMessage = JSON.stringify(message);
